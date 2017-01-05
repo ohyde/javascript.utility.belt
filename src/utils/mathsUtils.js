82b177700ef7de57abbb2121857ae133a755f8a1
ohyde <oliverhyde@gmail.com>
2017-01-05 15:37:49 +1300

    slowly refactoring isPrime

--- src/utils/mathsUtils.js
+++ src/utils/mathsUtils.js
@@ -1,22 +1,23 @@
 import R from 'ramda';
+import { moduloTwo } from './ramdaUtils';
 
 // maths
 export const gcd = (a, b) => !b ? a : gcd(b, a % b)
 export const lcm = (a, b) => (a * b) / gcd(a, b)
 export const equalsZero = R.equals(R.__, 0);
 
-export const isPrime = (num) => {
-    if(num <= 1) return false;
-    if(num == 2) return true;
-    if(num % 2 === 0) return false;
 
-    let i = 3;
-    while ((i * i) <= num) {
-        if(num%i===0)
-            return false;
-        else {
-          i +=2
-        }
-    }
-    return true;
+export const isPrime = (num) => {
+  if(R.or(R.lte(num, 1), equalsZero(moduloTwo(num)))) return false;
+  if(num == 2) return true;
+
+  let i = 3;
+  while ((i * i) <= num) {
+      if(num%i===0)
+          return false;
+      else {
+        i +=2
+      }
+  }
+  return true;
 }
