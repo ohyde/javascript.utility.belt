1347c1fc451c4eea97a66bce53a05d89f5ae0e25
ohyde <oliverhyde@gmail.com>
2017-06-28 14:30:04 +1200

    Update ramdaUtils.js

--- src/utils/ramdaUtils.js
+++ src/utils/ramdaUtils.js
@@ -6,6 +6,8 @@ export const isEven = R.compose(R.equals(0), moduloTwo);
 export const isOdd = R.compose(R.gt(R.__, 0), moduloTwo);
 export const defaultToUndefined = R.defaultTo(undefined);
 export const isObj = R.is(Object);
+export const arrGt = (arr, num = 0) => R.gt(R.length(arr), num);
+export const isNotEmptyArray = R.allPass([arrGt, R.isArrayLike]);
 export const isObjValidWithKeys = (obj, keys) => {
   return isObj(obj) && R.reduce((acc, x) => {
     if (R.not(acc)) return false;
