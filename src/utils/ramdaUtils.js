ec14b94f3e7b208cdbbf414883279685da06d84e
ohyde <oliverhyde@gmail.com>
2017-07-13 14:39:28 +1200

    Update ramdaUtils.js

--- src/utils/ramdaUtils.js
+++ src/utils/ramdaUtils.js
@@ -30,3 +30,5 @@ export const filterByAttributeAllTrue = R.curry((conditions, arr) => {
 
 export const dotPath = R.useWith(R.path, [R.split('.')]);
 export const propsDotPath = R.useWith(R.ap, [R.map(dotPath), R.of]);
+
+export const arrayPath = (array, object) => R.view(R.lensPath(array), object);
