3b4249ae0a5020bc289f751147937ab35887f8a2
ohyde <oliverhyde@gmail.com>
2017-07-17 08:49:35 +1200

    Update ramdaUtils.js

--- src/utils/ramdaUtils.js
+++ src/utils/ramdaUtils.js
@@ -15,11 +15,8 @@ export const isObjValidWithKeys = (obj, keys) => {
   }, true, keys);
 }
 
-// const byTheNumberLessThan3 = {
-//  expr: R.gt(R.__, 0),
-//  prop: 'theNum'
-//}
-//filterByAttributeAllTrue([byTheNumberLessThan3], anArray) 
+// const byTheNumberLessThan3 = { expr: R.gt(R.__, 0), prop: 'theNum' }
+// filterByAttributeAllTrue([byTheNumberLessThan3], anArray) 
 export const filterByAttributeAllTrue = R.curry((conditions, arr) => {
   return R.filter((item) => {
     const results = R.map(eachCondition => eachCondition.expr(R.prop(eachCondition.prop, item)), conditions);
