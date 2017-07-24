b918e040ac65c024b85a086f8e40ff4596a71fb0
ohyde <oliverhyde@gmail.com>
2017-07-25 10:57:08 +1200

    Update objectUtils.js

--- src/utils/objectUtils.js
+++ src/utils/objectUtils.js
@@ -1,6 +1,8 @@
 import R from 'ramda';
+import jub from 'index';
 
 export const dotPath = R.useWith(R.path, [R.split('.')]);
 export const propsDotPath = R.useWith(R.ap, [R.map(dotPath), R.of]);
 export const arrayPath = (array, object) => R.view(R.lensPath(array), object);
 
+export const pathNotNil = R.pathSatisfies(x => jub.isNotNil(x)); 
