24a51594b24656ef158681774813ede9771e290c
ohyde <oliverhyde@gmail.com>
2017-07-28 13:16:26 +1200

    using jest now

--- src/utils/tests/numberUtils.spec.js
+++ src/utils/tests/numberUtils.spec.js
@@ -1,40 +1,39 @@
-import { expect } from 'chai';
-import jub from '../../src/index';
+import jub from 'index';
 
 describe('numberUtils', () => {
   describe("isEven", () => {
     it("should return true when 2 passed to it", () => {
       const result = jub.isEven(2);
-      expect(result).to.equal(true);
+      expect(result).toEqual(true);
     });
 
     it("should return false when 3 passed to it", () => {
       const result = jub.isEven(3);
-      expect(result).to.equal(false);
+      expect(result).toEqual(false);
     });
   });
 
   describe("isOdd", () => {
     it("should return false when 2 passed to it", () => {
     	const result = jub.isOdd(2);
-      expect(result).to.equal(false);
+      expect(result).toEqual(false);
     });
 
     it("should return true when 3 passed to it", () => {
     	const result = jub.isOdd(3);
-      expect(result).to.equal(true);
+      expect(result).toEqual(true);
     });
   });
 
   describe("moduloTwo", () => {
     it("should return 0 when 2 passed to it", () => {
     	const result = jub.moduloTwo(2);
-      expect(result).to.equal(0);
+      expect(result).toEqual(0);
     });
 
     it("should return 1 when 3 passed to it", () => {
     	const result = jub.moduloTwo(3);
-      expect(result).to.equal(1);
+      expect(result).toEqual(1);
     });
   });
 })
