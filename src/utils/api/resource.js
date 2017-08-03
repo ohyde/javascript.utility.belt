89e8aa6ec82101377808041408224ff6eb0339d7
ohyde <oliverhyde@gmail.com>
2017-08-03 16:46:27 +1200

    added some lamda api handler helpers

---
+++ src/utils/api/resource.js
@@ -0,0 +1,11 @@
+const methodGET = R.compose(R.equals('GET'), R.propOr(null, "method"))
+const methodPOST = R.compose(R.equals('POST'), R.propOr(null, "method"))
+const methodPUT = R.compose(R.equals('PUT'), R.propOr(null, "method"))
+const methodDELETE = R.compose(R.equals('DELETE'), R.propOr(null, "method"))
+
+const resourceBase = R.compose(R.equals('/'), R.propOr(null, "resource"))
+
+export const getAPI = R.allPass([methodGET, resourceBase]);
+export const postAPI = R.allPass([methodPOST, resourceBase]);
+export const putAPI = R.allPass([methodPUT, resourceBase]);
+export const deleteAPI = R.allPass([methodDELETE, resourceBase]);
