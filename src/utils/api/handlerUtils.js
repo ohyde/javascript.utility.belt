299ad4b41327423f2087572e20da844c15237f36
ohyde <oliverhyde@gmail.com>
2017-08-03 16:57:57 +1200

    tied the functions together

--- src/utils/api/handlerUtils.js
+++ src/utils/api/handlerUtils.js
@@ -1,14 +1,18 @@
+// Built for AWS Lambda Function Event Object
+
 const methodGET = R.compose(R.equals('GET'), R.propOr(null, "method"))
 const methodPOST = R.compose(R.equals('POST'), R.propOr(null, "method"))
 const methodPUT = R.compose(R.equals('PUT'), R.propOr(null, "method"))
 const methodDELETE = R.compose(R.equals('DELETE'), R.propOr(null, "method"))
 
-export const createResourceTest = (resource) => R.compose(resource, R.propOr(null, "resource"))
 const resourceBase = createResourceTest(R.equals('/'));
 const resourceHealthcheck = createResourceTest(R.startsWith('/healthcheck'));
+
+export const createResourceTest = (resource) => R.compose(resource, R.propOr(null, "resource"))
 
 // expect an object with resource and method keys e.g. { resource: '/', method: 'GET'g}
 export const getAPI = R.allPass([methodGET, resourceBase]);
 export const postAPI = R.allPass([methodPOST, resourceBase]);
 export const putAPI = R.allPass([methodPUT, resourceBase]);
 export const deleteAPI = R.allPass([methodDELETE, resourceBase]);
+export const healthcheckGetAPI = R.allPass([methodGET, resourceHealthcheck]);
