01d0eafbb126b7561cd69176265ebe8af7ec391d
ohyde <oliverhyde@gmail.com>
2017-08-03 16:50:27 +1200

    created resource generator

--- src/utils/api/handlerUtils.js
+++ src/utils/api/handlerUtils.js
@@ -4,8 +4,9 @@ const methodPUT = R.compose(R.equals('PUT'), R.propOr(null, "method"))
 const methodDELETE = R.compose(R.equals('DELETE'), R.propOr(null, "method"))
 
 const resourceBase = R.compose(R.equals('/'), R.propOr(null, "resource"))
+export const createResourceTest = (resource) => R.compose(R.equals(resource), R.propOr(null, "resource"))
 
-// expect an object with resource and method keys e.g. { resource: '/', method: 'GET'}
+// expect an object with resource and method keys e.g. { resource: '/', method: 'GET'g}
 export const getAPI = R.allPass([methodGET, resourceBase]);
 export const postAPI = R.allPass([methodPOST, resourceBase]);
 export const putAPI = R.allPass([methodPUT, resourceBase]);
