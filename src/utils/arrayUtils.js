2c51d471314758759e3c5b8ee5eb69960b4eb83f
ohyde <oliverhyde@gmail.com>
2017-08-25 11:48:30 +1200

    Added update object in array

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -2,3 +2,16 @@ import R from 'ramda';
 
 export const mapIndexed = R.addIndex(R.map);
 export const arrGt = (num = 0, arr) => R.gt(R.length(arr), num);
+
+export const updateObjectInArrayById = (existingArray, newObject) => {
+  const id = R.prop('id', newObject);
+  if(id === null) return existingArray; 
+  
+  const currentUserObjectIndex = R.findIndex(R.propEq('id', id), existingArray);
+  if(currentUserObjectIndex === -1) return existingArray;
+  
+  const currentUserObject = R.nth(currentUserObjectIndex, existingArray);
+  const newUserObject = R.merge(currentUserObject, newObject) ;
+  
+  return R.update(currentUserObjectIndex)(newUserObject)(existingArray);
+}
