c57a5f8008071845ca1fcffd4a72764b58d70183
ohyde <oliverhyde@gmail.com>
2017-08-25 14:18:06 +1200

    Update arrayUtils.js

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -3,11 +3,11 @@ import R from 'ramda';
 export const mapIndexed = R.addIndex(R.map);
 export const arrGt = (num = 0, arr) => R.gt(R.length(arr), num);
 
-export const updateObjectInArrayById = (existingArray, newObject) => {
-  const id = R.prop('id', newObject);
-  if(id === null) return existingArray; 
+export const updateObjectInArrayByPropEq = (existingArray, newObject, propMatch = 'id') => {
+  const newPropVal = R.propOr(null, propMatch, newObject);
+  if(newPropVal === null) return existingArray; 
   
-  const currentUserObjectIndex = R.findIndex(R.propEq('id', id), existingArray);
+  const currentUserObjectIndex = R.findIndex(R.propEq(propMatch, newPropVal), existingArray);
   if(currentUserObjectIndex === -1) return existingArray;
   
   const currentUserObject = R.nth(currentUserObjectIndex, existingArray);
