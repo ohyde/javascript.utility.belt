165ae7308306332b65ef35d505181416f683a817
Oliver Hyde <oly@olivers-mbp.home>
2018-06-24 17:34:27 +0100

    cleaned duplicated code from search and replace

--- src/utils/miscUtils.js
+++ src/utils/miscUtils.js
@@ -6,21 +6,22 @@ export const defaultToUndefined = R.defaultTo(undefined);
 
 
 // searchAndReplace(R.startsWith('reference_'), documentOne)
-export const searchAndReplace = (needleFunc, obj, replaceFunc) => {
+export const searchAndReplace = (needleFunc, replaceFunc, obj) => {
   const isObj = R.compose(R.equals('Object'), R.type);
   const isArray = R.compose(R.equals('Array'), R.type); 
+  const curriedSearchAndReplace = R.curry(searchAndReplace)(needleFunc, replaceFunc);
 
   if (isObj(obj)) {
     const result = R.reduce((acc, key) => {
       if (needleFunc(key)) {
-        if (isArray(obj[key]))
+        if (isArray(obj[key])) {
           const replaced = R.map(replaceFunc, obj[key]);
           return R.merge(acc, { [key]: replaced })
         }
       }
       
-      if (isObj(obj[key])) return R.merge(acc, { [key]: searchAndReplace(needleFunc, obj[key]) })
-      if (isArray(obj[key])) return R.merge(acc, { [key]: searchAndReplace(needleFunc, obj[key]) })
+      if (isObj(obj[key])) return R.merge(acc, { [key]: curriedSearchAndReplace(obj[key]) })
+      if (isArray(obj[key])) return R.merge(acc, { [key]: curriedSearchAndReplace(obj[key]) })
       
       return R.merge(acc, { [key]: obj[key] });
     }, {}, R.keys(obj));
@@ -30,11 +31,10 @@ export const searchAndReplace = (needleFunc, obj, replaceFunc) => {
    
   if (R.type(obj) === 'Array') {
     return R.map((item) => {
-      if (R.or(isObj(item), isArray(item))) return searchAndReplace(needleFunc, item);
+      if (R.or(isObj(item), isArray(item))) return curriedSearchAndReplace(item);
       return item;
     }, obj);
   }
   
   return obj;
 }
-
