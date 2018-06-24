c4c9cccb37e96e95ee1912412d740f8ce65f8aed
Oliver Hyde <oly@olivers-mbp.home>
2018-06-24 17:39:42 +0100

    abstracted search and replace logic out

---
+++ src/utils/misc/searchAndReplace.js
@@ -0,0 +1,37 @@
+const isObj = R.compose(R.equals('Object'), R.type);
+const isArray = R.compose(R.equals('Array'), R.type); 
+
+
+const objSearch = (needleFunc, replaceFunc, obj) => {
+  const result = R.reduce((acc, key) => {
+    if (needleFunc(key)) {
+      if (isArray(obj[key])) {
+        const replaced = R.map(replaceFunc, obj[key]);
+        return R.merge(acc, { [key]: replaced })
+      }
+    }
+    
+    if (isObj(obj[key])) return R.merge(acc, { [key]: searchAndReplace(needleFunc, replaceFunc, obj[key]) })
+    if (isArray(obj[key])) return R.merge(acc, { [key]: searchAndReplace(needleFunc, replaceFunc, obj[key]) })
+    
+    return R.merge(acc, { [key]: obj[key] });
+  }, {}, R.keys(obj));
+  
+  return result;
+}
+
+const arraySearch = (needleFunc, replaceFunc, arr) => {
+  return R.map((item) => {
+    if (R.or(isObj(item), isArray(item))) return searchAndReplace(needleFunc, replaceFunc, item);
+    return item;
+  }, arr);
+}
+
+
+// searchAndReplace(R.startsWith('reference_'), documentOne)
+const searchAndReplace = (needleFunc, replaceFunc, obj) => {
+  if (isObj(obj)) { return objSearch(needleFunc, replaceFunc, obj) }
+  if (isArray(obj)) { return arraySearch(needleFunc, replaceFunc, obj) }
+  
+  return obj;
+}
--- src/utils/miscUtils.js
+++ src/utils/miscUtils.js
@@ -4,37 +4,3 @@ export const taplog = R.tap(console.log);
 export const defaultToUndefined = R.defaultTo(undefined);
 
 
-
-// searchAndReplace(R.startsWith('reference_'), documentOne)
-export const searchAndReplace = (needleFunc, replaceFunc, obj) => {
-  const isObj = R.compose(R.equals('Object'), R.type);
-  const isArray = R.compose(R.equals('Array'), R.type); 
-  const curriedSearchAndReplace = R.curry(searchAndReplace)(needleFunc, replaceFunc);
-
-  if (isObj(obj)) {
-    const result = R.reduce((acc, key) => {
-      if (needleFunc(key)) {
-        if (isArray(obj[key])) {
-          const replaced = R.map(replaceFunc, obj[key]);
-          return R.merge(acc, { [key]: replaced })
-        }
-      }
-      
-      if (isObj(obj[key])) return R.merge(acc, { [key]: curriedSearchAndReplace(obj[key]) })
-      if (isArray(obj[key])) return R.merge(acc, { [key]: curriedSearchAndReplace(obj[key]) })
-      
-      return R.merge(acc, { [key]: obj[key] });
-    }, {}, R.keys(obj));
-    
-    return result;
-  }
-   
-  if (R.type(obj) === 'Array') {
-    return R.map((item) => {
-      if (R.or(isObj(item), isArray(item))) return curriedSearchAndReplace(item);
-      return item;
-    }, obj);
-  }
-  
-  return obj;
-}
