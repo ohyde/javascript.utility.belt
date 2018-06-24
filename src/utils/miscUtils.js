e9229634ad882c38938548fc69a63a8b8de06094
Oliver Hyde <oly@olivers-mbp.home>
2018-06-24 17:30:40 +0100

    clean up tabs and spaces

--- src/utils/miscUtils.js
+++ src/utils/miscUtils.js
@@ -7,15 +7,15 @@ export const defaultToUndefined = R.defaultTo(undefined);
 
 // searchAndReplace(R.startsWith('reference_'), documentOne)
 export const searchAndReplace = (needleFunc, obj, replaceFunc) => {
-	const isObj = R.compose(R.equals('Object'), R.type);
-	const isArray = R.compose(R.equals('Array'), R.type);	
+  const isObj = R.compose(R.equals('Object'), R.type);
+  const isArray = R.compose(R.equals('Array'), R.type); 
 
   if (isObj(obj)) {
     const result = R.reduce((acc, key) => {
       if (needleFunc(key)) {
-      	if (isArray(obj[key]))
-        	const replaced = R.map(replaceFunc, obj[key]);
-        	return R.merge(acc, { [key]: replaced })
+        if (isArray(obj[key]))
+          const replaced = R.map(replaceFunc, obj[key]);
+          return R.merge(acc, { [key]: replaced })
         }
       }
       
